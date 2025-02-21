b970751d39ac016c2d79c45154305c72864ff8d1
Tuukka <tuukka.rintala@tuni.fi>
2025-02-21 21:10:58 +0200

    Worst implementation of a grid I could think of.

---
+++ futurice-game-jam/Scripts/Grid.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+namespace Transparency
+{
+    public partial class Grid : Node2D
+    {
+        private Cell[,] _cells = null;
+        [Export] private int _cellWidth = 16;
+        [Export] private int _cellHeight = 16;
+        [Export] private int _width = 16;
+        [Export] private int _height = 16;
+        public int Height {get {return _height;}}
+        public int Width {get {return _width;}}
+        public override void _Ready()
+        {
+            _cells = new Cell[_width,_height];
+            int l = 0;
+            for (int i = 0; i < _width; i++)
+            {
+                for (int j = 0; j < _height; j++)
+                {
+                    _cells[i, j] = GetChild(l, false) as Cell;
+                    _cells[i, j].GridPosition = new Vector2I(i, j);
+                    _cells[i, j].GlobalPosition = new Vector2I(i * _cellWidth, j * _cellHeight);
+                    l++;
+                }
+            }
+        }
+    }
+}
