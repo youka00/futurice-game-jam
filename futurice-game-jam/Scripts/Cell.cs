f87be96c3e9b9f8fd9881066236b5324cbc730d4
Tuukka <tuukka.rintala@tuni.fi>
2025-02-22 22:20:18 +0200

    Futzing around trying to fx the spawning of blocks.

--- futurice-game-jam/Scripts/Cell.cs
+++ futurice-game-jam/Scripts/Cell.cs
@@ -12,6 +12,7 @@ namespace Transparency
     public partial class Cell : Node2D
     {
         private string _rockScene = "res://rock.tscn";
+        private string _steelScene = "res://steel.tscn";
         private Vector2I _gridPosition = Vector2I.Zero;
         public Vector2I GridPosition{get {return _gridPosition;} set {_gridPosition = value;}}
         private bool _collidable = false;
@@ -20,15 +21,25 @@ namespace Transparency
         public List<Occupier> Occupiers {get {return _occupiers;}}
         public void CreateOccupier(OccupierScene occupierScene)
         {
+            PackedScene scene = null;
             switch (occupierScene)
             {
                 case OccupierScene.Rock:
-                PackedScene scene = ResourceLoader.Load<PackedScene>(_rockScene);
+                scene = ResourceLoader.Load<PackedScene>(_rockScene);
                 Rock rock = scene.Instantiate<Rock>();
                 GD.Print(GridPosition);
-                AddChild(rock);
+                rock.GlobalPosition = GlobalPosition;
                 GD.Print(rock.GlobalPosition);
                 _occupiers.Add(rock);
+                _collidable = true;
+                break;
+                case OccupierScene.Steel:
+                scene = ResourceLoader.Load<PackedScene>(_rockScene);
+                Steel steel = scene.Instantiate<Steel>();
+                GD.Print(GridPosition);
+                steel.GlobalPosition = GlobalPosition;
+                GD.Print(steel.GlobalPosition);
+                _occupiers.Add(steel);
                 _collidable = true;
                 break;
             }
