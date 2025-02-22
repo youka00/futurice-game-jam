7bd1420b30933ccb4ac42ca8aae32f3396e31431
Youka <uluozmelisadilan@gmail.com>
2025-02-22 16:33:07 +0200

    Now we have esc menu where we have resume restart and mainmenu. Also we have a ghost but it doesnt move right for now

---
+++ futurice-game-jam/Scripts/HaamuMovement.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class HaamuMovement : CharacterBody2D
+{
+	[Export] public float Speed = 100f;
+	[Export] public float LeftLimit = -200f;
+	[Export] public float RightLimit = 200f;
+
+	private Vector2 _Velocity = new Vector2(1, 0);
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Position += _Velocity * Speed * (float)delta;
+		if (Position.X < LeftLimit || Position.X > RightLimit)
+		{
+			_Velocity.X *= -1;
+		}
+	}
+}
