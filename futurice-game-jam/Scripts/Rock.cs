84ec738d420ef33b5bd1c36a22518b4baf90a8f4
Tuukka <tuukka.rintala@tuni.fi>
2025-02-23 14:45:16 +0200

    rock texture for main level edges and randomised rock texture.

--- futurice-game-jam/Scripts/Rock.cs
+++ futurice-game-jam/Scripts/Rock.cs
@@ -12,6 +12,7 @@ namespace Transparency
         // Called when the node enters the scene tree for the first time.
         public override void _Ready()
         {
+            Frame = GD.RandRange(0 , 2);
         }
 
         // Called every frame. 'delta' is the elapsed time since the previous frame.
