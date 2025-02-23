d49c8b12775c9d0e1364ea25be50bebfd71ea6d1
Youka <uluozmelisadilan@gmail.com>
2025-02-23 10:40:06 +0200

    now lights shows ghosts with "same" color

--- futurice-game-jam/Scripts/Player.cs
+++ futurice-game-jam/Scripts/Player.cs
@@ -1,20 +1,26 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 namespace Transparency
 {
     public partial class Player : Area2D, Occupier
     {
         [Export] private float _speed = 20;
+
+        [Export] private CharacterBody2D characterBody2D; // Red Ghost
+        [Export] private CharacterBody2D blue; // Blue Ghost
+        [Export] private CharacterBody2D green; // Green Ghost
+
         private Vector2I _gridPosition = Vector2I.Zero;
-        public Vector2I GridPosition{get {return _gridPosition;} set {_gridPosition = value;}}
-
+        public Vector2I GridPosition { get { return _gridPosition; } set { _gridPosition = value; } }
         public CellOccupierType Type => CellOccupierType.Player;
-
         public Color AffectedByColor => Color.None;
+
         private string _redScenePath = "res://red_light.tscn";
         private string _greenScenePath = "res://green_light.tscn";
         private string _blueScenePath = "res://blue_light.tscn";
+
         private PackedScene _redScene = null;
         private PackedScene _greenScene = null;
         private PackedScene _blueScene = null;
@@ -28,7 +34,7 @@ namespace Transparency
             Left,
             Right,
         }
-        // Called when the node enters the scene tree for the first time.
+
         public override void _Ready()
         {
             _redScene = ResourceLoader.Load<PackedScene>(_redScenePath);
@@ -36,94 +42,101 @@ namespace Transparency
             _greenScene = ResourceLoader.Load<PackedScene>(_greenScenePath);
         }
 
-        // Called every frame. 'delta' is the elapsed time since the previous frame.
         public override void _Process(double delta)
         {
+            UpdateGhostVisibility();
             ReadInput();
         }
-        private void ReadInput()
+
+        private bool IsInRange()
         {
-            if (Input.IsActionJustPressed("MoveUp"))
+            return characterBody2D.GlobalPosition.DistanceTo(GlobalPosition) <= 0 ||
+                   blue.GlobalPosition.DistanceTo(GlobalPosition) <= 0 ||
+                   green.GlobalPosition.DistanceTo(GlobalPosition) <= 0;
+        }
+
+        private void UpdateGhostVisibility()
+        {
+            if (IsInRange())
             {
-                Move(Vector2I.Up);
+                blue.Visible = true;
+                green.Visible = true;
+                characterBody2D.Visible = true;
             }
-            if (Input.IsActionJustPressed("MoveDown"))
+            else
             {
-                Move(Vector2I.Down);
-            }
-            if (Input.IsActionJustPressed("MoveLeft"))
-            {
-                Move(Vector2I.Left);
-            }
-            if (Input.IsActionJustPressed("MoveRight"))
-            {
-                Move(Vector2I.Right);
-            }
-            if (Input.IsActionJustPressed("Red"))
-            {
-                if (_light != null)
+                if (_light is BlueLight blueLight)
                 {
-                    _light.RemoveLightCollision();
-                    _light.QueueFree();
-                    _light = null;
-                    _light = _redScene.Instantiate<RedLight>();
-                    AddChild(_light);
-                    _light.Position = Vector2.Zero;
-                    _light.SetAffectedArea(_gridPosition);
+                    blue.Visible = blueLight.IsInLightArea(blue.GlobalPosition);
+                    green.Visible = false;
+                    characterBody2D.Visible = false;
                 }
-                else if (_light is RedLight)
-                {}
+                else if (_light is GreenLight greenLight)
+                {
+                    green.Visible = greenLight.IsInLightArea(green.GlobalPosition);
+                    blue.Visible = false;
+                    characterBody2D.Visible = false;
+                }
+                else if (_light is RedLight redLight)
+                {
+                    characterBody2D.Visible = redLight.IsInLightArea(characterBody2D.GlobalPosition);
+                    blue.Visible = false;
+                    green.Visible = false;
+                }
                 else
                 {
-                    _light = _redScene.Instantiate<RedLight>();
-                    AddChild(_light);
-                    _light.Position = Vector2.Zero;
-                    _light.SetAffectedArea(_gridPosition);
-                }
-            }
-            if (Input.IsActionJustPressed("Green"))
-            {
-                if (_light != null)
-                {
-                    _light.RemoveLightCollision();
-                    _light.QueueFree();
-                    _light = null;
-                    _light = _greenScene.Instantiate<GreenLight>();
-                    AddChild(_light);
-                    _light.Position = Vector2.Zero;
-                    _light.SetAffectedArea(_gridPosition);
-                }
-                else if (_light is GreenLight)
-                {}
-                else
-                {
-                    _light = _greenScene.Instantiate<GreenLight>();
-                    AddChild(_light);
-                    _light.Position = Vector2.Zero;
-                    _light.SetAffectedArea(_gridPosition);
-                }
-            }
-            if (Input.IsActionJustPressed("Blue"))
-            {
-                if (_light != null)
-                {
-                    _light.RemoveLightCollision();
-                    _light.QueueFree();
-                    _light = null;
-                    _light = _blueScene.Instantiate<BlueLight>();
-                    AddChild(_light);
-                    _light.SetAffectedArea(_gridPosition);
-                }
-                else if (_light is BlueLight)
-                {}
-                else
-                {
-                    _light = _blueScene.Instantiate<BlueLight>();
-                    AddChild(_light);
-                    _light.SetAffectedArea(_gridPosition);
+                    blue.Visible = false;
+                    green.Visible = false;
+                    characterBody2D.Visible = false;
                 }
             }
         }
+
+        private void ReadInput()
+        {
+            if (Input.IsActionJustPressed("MoveUp")) Move(Vector2I.Up);
+            if (Input.IsActionJustPressed("MoveDown")) Move(Vector2I.Down);
+            if (Input.IsActionJustPressed("MoveLeft")) Move(Vector2I.Left);
+            if (Input.IsActionJustPressed("MoveRight")) Move(Vector2I.Right);
+
+            if (Input.IsActionJustPressed("Red")) ChangeLight(_redScene, typeof(RedLight));
+            if (Input.IsActionJustPressed("Green")) ChangeLight(_greenScene, typeof(GreenLight));
+            if (Input.IsActionJustPressed("Blue")) ChangeLight(_blueScene, typeof(BlueLight));
+        }
+
+        private void ChangeLight(PackedScene newLightScene, Type lightType)
+        {
+            if (_light != null)
+            {
+                _light.QueueFree();
+                _light = null;
+            }
+
+            _light = newLightScene.Instantiate() as Light;
+
+            if (_light != null)
+            {
+                AddChild(_light);
+                _light.Position = Vector2.Zero;
+                _light.SetAffectedArea(_gridPosition);
+
+                // Connect signals for RedLight
+                if (_light is RedLight redLight)
+                {
+                    redLight.Connect("GhostEntered", Callable.From<Node2D>(OnGhostEnteredRedLight));
+                    redLight.Connect("GhostExited", Callable.From<Node2D>(OnGhostExitedRedLight));
+                }
+                // Connect signals for GreenLight
+                else if (_light is GreenLight greenLight)
+                {
+                    greenLight.Connect("GhostEntered", Callable.From<Node2D>(OnGhostEnteredGreenLight));
+                    greenLight.Connect("GhostExited", Callable.From<Node2D>(OnGhostExitedGreenLight));
+                }
+            }
+
+            UpdateGhostVisibility(); // Immediately update ghost visibility
+        }
+
         private void Move(Vector2I direction)
         {
             if (!CheckCollision(_gridPosition + direction))
@@ -132,14 +145,18 @@ namespace Transparency
                 _gridPosition += direction;
             }
         }
-        private bool CheckCollision(Vector2I gridposition)
+
+        private bool CheckCollision(Vector2I gridPosition)
         {
-            if (gridposition.X < 0 || gridposition.Y < 0 || gridposition.X > Level.Current.CurrentGrid.Width - 1 || gridposition.Y > Level.Current.CurrentGrid.Height - 1)
+            if (gridPosition.X < 0 || gridPosition.Y < 0 ||
+                gridPosition.X > Level.Current.CurrentGrid.Width - 1 ||
+                gridPosition.Y > Level.Current.CurrentGrid.Height - 1)
             {
                 return true;
             }
-            return Level.Current.CurrentGrid.Cells[gridposition.X, gridposition.Y].Collidable;;
+            return Level.Current.CurrentGrid.Cells[gridPosition.X, gridPosition.Y].Collidable;
         }
+
         public void CollideWith(Occupier occupier)
         {
             switch (occupier.Type)
@@ -149,6 +166,31 @@ namespace Transparency
                 case CellOccupierType.Mineable: break;
                 case CellOccupierType.Obstacle: break;
             }
+        }
+
+        // Ensure Signal Methods Exist
+        private void OnGhostEnteredRedLight(Node2D haamu)
+        {
+            GD.Print("Red Ghost entered red light!");
+            haamu.Visible = true;
+        }
+
+        private void OnGhostExitedRedLight(Node2D haamu)
+        {
+            GD.Print("Red Ghost exited red light!");
+            haamu.Visible = false;
+        }
+
+        private void OnGhostEnteredGreenLight(Node2D haamu)
+        {
+            GD.Print("Green Ghost entered green light!");
+            haamu.Visible = true;
+        }
+
+        private void OnGhostExitedGreenLight(Node2D haamu)
+        {
+            GD.Print("Green Ghost exited green light!");
+            haamu.Visible = false;
         }
     }
 }
